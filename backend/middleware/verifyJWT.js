eed37374fa836e06ed68699148e7ffca172f0cfe
574515 <574515c0d1ng@gmail.com>
2024-10-09 18:01:26 +0200

    Added JWT verifier and token and cookie generator

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'
+import User from "../models/userModel.js";
+
+const protectRoute = async (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (!token) return res.status(401).json({ error: 'Unauthorized.', action: 'logout' });
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded.userId, null, null).select('-password');
+    next();
+}
+
+export default protectRoute;
