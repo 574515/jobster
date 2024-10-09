12b272e19017f21a2c986da2d2c92581c25db675
574515 <574515c0d1ng@gmail.com>
2024-10-09 22:38:07 +0200

    Made custom color mode switch

---
+++ frontend/src/components/CustomColorModeSwitch.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {useColorMode} from '@chakra-ui/react';
+import {MoonIcon, SunIcon} from '@chakra-ui/icons';
+import {CustomColorModeIcon} from "../models/componentsTypes.ts";
+
+const CustomColorModeSwitch = () => {
+	const {colorMode, toggleColorMode} = useColorMode();
+	const [isDarkMode, setIsDarkMode] = React.useState<boolean>(colorMode === 'dark');
+	const iconProps: CustomColorModeIcon = {
+		fontSize: '1.5rem',
+		onClick: () => toggleColorMode(),
+		className: 'make-pointer',
+		me: 'auto',
+	}
+
+	React.useEffect(() => {
+		setIsDarkMode(colorMode === 'dark');
+	}, [colorMode]);
+
+	return (
+		isDarkMode ?
+			<SunIcon {...iconProps} /> :
+			<MoonIcon {...iconProps} />
+	);
+}
+
+export default CustomColorModeSwitch;
