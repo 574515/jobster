812984c11b5292ed5e1f0ced2e989a433afb4d71
574515 <574515c0d1ng@gmail.com>
2024-10-09 22:19:43 +0200

    Added auth instance

--- frontend/src/api/axiosInstances.ts
+++ frontend/src/api/axiosInstances.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-export const collectApiInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_ENDPOINT,
-})
-
-export const usersInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_API_USERS_URL,
-    withCredentials: true,
+export const authInstance = axios.create({
+	baseURL: import.meta.env.VITE_AUTH_URL,
+	withCredentials: true,
 });
