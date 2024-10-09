af9491cbb5d0654c61ed32ae93af4c0b8f9d32c2
574515 <574515c0d1ng@gmail.com>
2024-10-09 22:18:34 +0200

    Added Auth page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,7 +1,14 @@
+import authScreenAtom from '../atoms/authScreenAtom.ts';
+import SignupCard from "../components/SignupCard.tsx";
+import LoginCard from "../components/LoginCard.tsx";
+
+import {useRecoilValue} from 'recoil';
+
+import '../styles/pagesStyle.css'
+
 const Auth = () => {
-    return (
-        "Auth"
-    );
+	const authScreenState = useRecoilValue(authScreenAtom);
+	return (authScreenState === 'login' ? <LoginCard/> : <SignupCard/>);
 }
 
 export default Auth;
