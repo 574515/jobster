1a5b8b593fe0307951897b75cce26297c16bfb07
574515 <574515c0d1ng@gmail.com>
2024-10-09 22:21:49 +0200

    Added form provider props

--- frontend/src/models/interfaces.ts
+++ frontend/src/models/interfaces.ts
@@ -1,5 +1,11 @@
 import React from "react";
+import {FieldValues, UseFormReturn} from "react-hook-form";
 
 export interface AuthProviderProps {
 	children: React.ReactNode;
+}
+
+export interface CustomFormProviderProps<T extends FieldValues> {
+	children: React.ReactNode;
+	formProviderData: UseFormReturn<T>;
 }
