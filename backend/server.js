53acf8fe62346040b96c0c9063cfa0bce5bc7509
574515 <574515c0d1ng@gmail.com>
2024-10-09 18:39:29 +0200

    Added routes and controllers for login, signup and logout

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,17 @@ import cookieParser from "cookie-parser";
 import corsOptions from "./config/corsOptions.js";
 import cors from 'cors';
 import connectDB from "./config/connectDB.js";
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 7002;
 void connectDB();
 
 app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
