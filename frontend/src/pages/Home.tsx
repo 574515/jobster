3bc38179b0fa59fba233c86284bc9fd5afefc05d
574515 <574515c0d1ng@gmail.com>
2024-10-21 20:40:03 +0200

    First iteration of Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,41 @@
+import React from "react";
+
+import userAtom from "../atoms/userAtom.ts";
+
+import {Flex, Text, useColorMode} from "@chakra-ui/react";
+import {useRecoilValue} from "recoil";
+
 const Home = () => {
+	const user = useRecoilValue(userAtom);
+	const {colorMode} = useColorMode();
+	const [isDarkMode, setIsDarkMode] = React.useState<boolean>(false);
+	const [flexBackground, setFlexBackground] = React.useState<string>("");
+
+	React.useEffect(() => {
+		setIsDarkMode(colorMode === "dark");
+	}, [colorMode])
+
+	React.useEffect(() => {
+		setFlexBackground(isDarkMode ? "gray.700" : "gray.300");
+	}, [isDarkMode]);
+
 	return (
-		"Home"
+		<Flex
+			border={'3px solid red'}
+			width={'25vw'}
+			height={'50vh'}
+			my={'2rem'}
+			justifyContent={'center'}
+			bg={flexBackground}
+		>
+			{user &&
+                <Text
+                    my={'1rem'}
+                >
+                    Hi, <a href={"/"}>{user.username}</a>
+                </Text>
+			}
+		</Flex>
 	);
 }
 
