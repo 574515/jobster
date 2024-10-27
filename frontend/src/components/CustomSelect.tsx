db51a750e331406c39bce591962642aca76ef830
574515 <574515c0d1ng@gmail.com>
2024-10-27 02:56:27 +0200

    Added custom select

---
+++ frontend/src/components/CustomSelect.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {FormControl, FormErrorMessage, FormLabel} from "@chakra-ui/react";
+import {Select} from "chakra-react-select";
+import {Controller} from "react-hook-form";
+import {CustomSelectProps} from "../models/interfaces.ts";
+import {AddEditJobNameType} from "../models/componentsTypes.ts";
+
+const CustomSelect: React.FC<CustomSelectProps> = (
+	{
+		choices, control, name, py, className,
+		label, definedValue,
+		isClearable = true
+	}
+) => {
+	return (
+		<Controller
+			control={control}
+			name={name as AddEditJobNameType}
+			render={({
+				         field: {
+					         onChange,
+					         onBlur,
+					         name,
+					         ref,
+				         },
+				         fieldState: {error}
+			         }) => (
+				<FormControl
+					py={py ? py : 0}
+					className={className}
+					isInvalid={!!error}
+					id={name}
+				>
+					{label && <FormLabel>{label}</FormLabel>}
+					<Select
+						name={name}
+						ref={ref}
+						onChange={onChange}
+						onBlur={onBlur}
+						options={choices}
+						value={definedValue}
+						isClearable={isClearable}
+					/>
+					<FormErrorMessage>{error && error.message}</FormErrorMessage>
+				</FormControl>
+			)}
+		/>
+	);
+}
+
+export default CustomSelect;
