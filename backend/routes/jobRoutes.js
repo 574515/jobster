903dfd72e9d41471e12bc540f79b4e2fd9d74c73
574515 <574515c0d1ng@gmail.com>
2024-10-27 02:00:34 +0100

    Added routes

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { newPost } from "../controllers/jobController.js";
+import { allPosts, deletePost, newPost } from "../controllers/jobController.js";
+import protectRoute from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
-router.post("/new", newPost);
+router.post("/new", newPost, protectRoute);
+router.get("/all/:userId", allPosts, protectRoute);
+router.delete("/listing/:_id", deletePost, protectRoute);
 
 export default router;
