707d8246e9584093025c1a7746130c465513161b
574515 <574515c0d1ng@gmail.com>
2024-10-27 02:26:25 +0200

    Made custom date selection

---
+++ frontend/src/components/CustomDateSelect.tsx
@@ -0,0 +1,40 @@
+import {FormControl, FormLabel} from "@chakra-ui/react";
+import {SingleDatepicker} from "chakra-dayzed-datepicker";
+import {TIME_FORMATS} from "../helpers/dateLocales.ts";
+import {Controller} from "react-hook-form";
+import {useRecoilValue} from "recoil";
+import userLocaleAtom from "../atoms/userLocaleAtom.ts";
+import React from "react";
+import {CustomDateSelectProps} from "../models/interfaces.ts";
+import {AddEditJobNameProps} from "../models/componentsTypes.ts";
+
+const CustomDateSelect: React.FC<CustomDateSelectProps> = (
+	{control, name, py, className, label, definedDate, setDate}
+) => {
+	const userLocale = useRecoilValue(userLocaleAtom);
+	return (
+		<Controller
+			control={control}
+			name={name as AddEditJobNameProps}
+			render={({field}) => (
+				<FormControl
+					py={py ?? 0}
+					className={className}
+					textAlign={"center"}
+				>
+					{label && <FormLabel mx={0} textAlign={"center"}>{label}</FormLabel>}
+					<SingleDatepicker
+						date={(field.value as Date) ?? definedDate}
+						onDateChange={(date) => {
+							field.onChange(date);
+							setDate(date);
+						}}
+						configs={{dateFormat: TIME_FORMATS[userLocale],}}
+					/>
+				</FormControl>
+			)}
+		/>
+	);
+}
+
+export default CustomDateSelect;
