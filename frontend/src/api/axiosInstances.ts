89ff6ed9aa7afa15fdb1086dca12a2ec52160fb3
574515 <574515c0d1ng@gmail.com>
2024-10-27 02:14:03 +0200

    Added job instance

--- frontend/src/api/axiosInstances.ts
+++ frontend/src/api/axiosInstances.ts
@@ -4,3 +4,8 @@ export const authInstance = axios.create({
 	baseURL: import.meta.env.VITE_AUTH_URL,
 	withCredentials: true,
 });
+
+export const jobInstance = axios.create({
+	baseURL: import.meta.env.VITE_JOB_URL,
+	withCredentials: true,
+});
--- frontend/src/models/interfaces.ts
+++ frontend/src/models/interfaces.ts
@@ -1,5 +1,13 @@
 import React from "react";
-import {FieldValues, UseFormReturn} from "react-hook-form";
+import {Control, FieldValues, UseFormReturn} from "react-hook-form";
+import {
+	AddEditJobNameProps,
+	AddEditJobProps,
+	AllJobsResponseModel,
+	BasicModalSelectTypeLV,
+	FiltersType
+} from "./componentsTypes.ts";
+import {CustomUser} from "./contextTypes.ts";
 
 export interface AuthProviderProps {
 	children: React.ReactNode;
@@ -8,4 +16,74 @@ export interface AuthProviderProps {
 export interface CustomFormProviderProps<T extends FieldValues> {
 	children: React.ReactNode;
 	formProviderData: UseFormReturn<T>;
+}
+
+export interface CustomJobCardProps {
+	userJob: AllJobsResponseModel;
+
+	setIsLoading(isLoading: boolean): void;
+
+	getAllListings(): void;
+}
+
+export interface AddEditJobModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	editing: boolean;
+	user: CustomUser;
+	methods: UseFormReturn<AddEditJobProps, any, undefined>;
+
+	getAllListings(): void;
+}
+
+export interface LoadingOverlayProps {
+	isLoading: boolean;
+}
+
+type CustomSelectionProps = {
+	control: Control<AddEditJobProps>;
+	name: AddEditJobNameProps;
+	py?: number;
+	className: string;
+	label?: string;
+}
+
+export interface CustomDateSelectProps extends CustomSelectionProps {
+	definedDate: Date;
+
+	setDate(date: Date): void;
+}
+
+export interface CustomSelectProps extends CustomSelectionProps {
+	choices: BasicModalSelectTypeLV[];
+	definedValue?: BasicModalSelectTypeLV;
+	isClearable?: boolean;
+	disabled?: boolean;
+}
+
+export interface MyJobsProps {
+	isLoading: boolean;
+	userJobListings: AllJobsResponseModel[];
+
+	setIsLoading(isLoading: boolean): void;
+
+	getAllListings(): void;
+}
+
+export interface StatusListProps {
+	jobListings: AllJobsResponseModel[];
+	checkedStates: Record<string, boolean>;
+
+	setCheckedStates: (updater: (prevState: Record<string, boolean>) => Record<string, boolean>) => void;
+}
+
+export interface FilterProps {
+
+	checkedStates: any;
+	setCheckedStates(e: ): void;
+userJobListings={userJobListings}
+}
+
+export interface StatusFiltersProps {
+	listOfFilters: FiltersType[];
 }
