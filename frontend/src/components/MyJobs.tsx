a2e964b4f4907f836868549d9a54187940680905
574515 <574515c0d1ng@gmail.com>
2024-10-27 02:56:05 +0200

    Added component for my jobs

---
+++ frontend/src/components/MyJobs.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import CustomJobCard from "./CustomJobCard.tsx";
+
+import {Box, Divider, Grid, VStack} from "@chakra-ui/react";
+import {AllJobsResponseModel} from "../models/componentsTypes.ts";
+import {MyJobsProps} from "../models/interfaces.ts";
+
+const MyJobs: React.FC<MyJobsProps> = (
+	{userJobListings, isLoading, setIsLoading, getAllListings}
+) => {
+	return (
+		<VStack minW={'75%'}>
+			<Divider/>
+			<Grid templateColumns='repeat(3, 1fr)' gap={5} w={"100%"}>
+				{(userJobListings.length > 0) && !isLoading && userJobListings
+					.map((userJob: AllJobsResponseModel, index: number) =>
+						<Box key={index}>
+							<CustomJobCard
+								userJob={userJob}
+								setIsLoading={setIsLoading}
+								getAllListings={getAllListings}
+							/>
+						</Box>
+					)}
+			</Grid>
+		</VStack>
+	);
+};
+
+export default MyJobs;
