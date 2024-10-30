dbf9d6f0b2a8e96ef8e6865eb86ccd38ffa72aee
574515 <574515c0d1ng@gmail.com>
2024-10-30 21:55:27 +0100

    Refactor

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.name}@${conn.connection.host}:${conn.connection.port}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+    await mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log(`MongoDB Connected`))
+        .catch(error => {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        });
 };
 
 export default connectDB;
