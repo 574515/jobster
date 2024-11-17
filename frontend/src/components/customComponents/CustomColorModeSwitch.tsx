cc9f92eae7d23ea05f2549e588db239e80872372
574515 <574515c0d1ng@gmail.com>
2024-11-17 07:02:26 +0100

    Refactor

--- frontend/src/components/customComponents/CustomColorModeSwitch.tsx
+++ frontend/src/components/customComponents/CustomColorModeSwitch.tsx
@@ -2,27 +2,21 @@ import React from 'react';
 
 import {useColorMode} from '@chakra-ui/react';
 import {MoonIcon, SunIcon} from '@chakra-ui/icons';
-import {CustomColorModeIcon} from "../../models/componentsTypes.ts";
+import {CustomColorModeIconType} from "../../models/componentsTypes.ts";
 
 const CustomColorModeSwitch = () => {
 	const {colorMode, toggleColorMode} = useColorMode();
-	const [isDarkMode, setIsDarkMode] = React.useState<boolean>(colorMode === 'dark');
-	const iconProps: CustomColorModeIcon = {
+	const [isDarkMode, setIsDarkMode] = React.useState<boolean>();
+	const iconProps: CustomColorModeIconType = {
 		fontSize: '1.5rem',
 		onClick: () => toggleColorMode(),
 		className: 'make-pointer',
 		me: 'auto',
 	}
 
-	React.useEffect(() => {
-		setIsDarkMode(colorMode === 'dark');
-	}, [colorMode]);
+	React.useEffect(() => setIsDarkMode(colorMode === 'dark'), [colorMode]);
 
-	return (
-		isDarkMode ?
-			<SunIcon {...iconProps} /> :
-			<MoonIcon {...iconProps} />
-	);
+	return isDarkMode ? <SunIcon {...iconProps} /> : <MoonIcon {...iconProps} />;
 }
 
 export default CustomColorModeSwitch;
