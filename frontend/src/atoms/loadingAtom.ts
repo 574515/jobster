0072e3fba1d4b91ed5972ff643136121e52d7b81
574515 <574515c0d1ng@gmail.com>
2024-11-17 07:50:56 +0100

    Added custom delete hook

---
+++ frontend/src/atoms/loadingAtom.ts
@@ -0,0 +1,8 @@
+import {atom, RecoilState} from 'recoil';
+
+const loadingAtom: RecoilState<boolean> = atom({
+	key: 'loadingAtom',
+	default: false,
+});
+
+export default loadingAtom;
---
+++ frontend/src/hooks/useDeleteItem.ts
@@ -0,0 +1,40 @@
+import React from "react";
+
+import homeScreenAtom from "../atoms/homeScreenAtom.ts";
+import loadingAtom from "../atoms/loadingAtom.ts";
+
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {constantItemName} from "../helpers/constants.ts";
+import {toast} from "../helpers/customToast.ts";
+import {DeleteItemHookProps} from "../models/interfaces.ts";
+import {handleError} from "../helpers/errorHandler.ts";
+import {AxiosError} from "axios";
+import {ErrorResponseType} from "../models/componentsTypes.ts";
+import {DeleteItemType} from "../models/hookTypes.ts";
+
+const useDeleteItem = (
+	{item, deleteAction, getAllItems}: DeleteItemHookProps
+) => {
+	const homeScreenState = useRecoilValue<string>(homeScreenAtom);
+	const setIsLoading = useSetRecoilState<boolean>(loadingAtom);
+	const [itemName, setItemName] = React.useState<string>();
+
+	React.useEffect(() => setItemName(constantItemName[homeScreenState]), [homeScreenState]);
+
+	const handleDelete = React.useCallback(
+		async () => {
+			if (!item) return;
+			setIsLoading(true);
+			await deleteAction(item._id)
+				.then((response: DeleteItemType) => {
+					void toast(`${itemName} "${response.deletedProperty}" Deleted Successfully`, 'success');
+					getAllItems();
+				})
+				.catch((error: AxiosError<ErrorResponseType>) => handleError(error))
+				.finally(() => setIsLoading(false));
+		}, [item, setIsLoading, deleteAction, itemName, getAllItems]);
+
+	return {handleDelete};
+};
+
+export default useDeleteItem;
