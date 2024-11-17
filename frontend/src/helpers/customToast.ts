dba7d9d53ba324af25a514dd759f55a64e0f58ee
574515 <574515c0d1ng@gmail.com>
2024-11-17 06:43:19 +0100

    Refactor

--- frontend/src/helpers/customToast.ts
+++ frontend/src/helpers/customToast.ts
@@ -1,15 +1,17 @@
 import swal, {SweetAlertIcon} from 'sweetalert2';
 
-export const toast = (title: string, icon: SweetAlertIcon, timer = 3000, showCloseButton = false, html?: string) => {
-    return swal.fire({
-        title: title,
-        icon: icon,
-        toast: true,
-        timer: timer,
-        position: 'top-right',
-        timerProgressBar: true,
-        showConfirmButton: false,
-        showCloseButton: showCloseButton,
-        html: html,
-    });
+export const toast = (
+	title: string,
+	icon: SweetAlertIcon,
+	timer = 3000,
+	showCloseButton = false,
+	html?: string
+) => {
+	return swal.fire({
+		title, icon, timer, showCloseButton, html,
+		toast: true,
+		position: 'top-right',
+		timerProgressBar: true,
+		showConfirmButton: false,
+	});
 };
