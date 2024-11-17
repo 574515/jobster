987cdb3265e917c649fd618385b7a33ac7795742
574515 <574515c0d1ng@gmail.com>
2024-11-17 06:46:45 +0100

    Refactoring

--- frontend/src/components/LoadingOverlay.tsx
+++ frontend/src/components/LoadingOverlay.tsx
@@ -1,15 +1,15 @@
-import {PacmanLoader} from 'react-spinners';
 import React from "react";
+
+import {CircleLoader} from 'react-spinners';
 import {LoadingOverlayProps} from "../models/interfaces.ts";
+
 import '../styles/componentStyle.css';
 
-const LoadingOverlay: React.FC<LoadingOverlayProps> = (
-	{isLoading,}
-) => {
+const LoadingOverlay: React.FC<LoadingOverlayProps> = ({isLoading}) => {
 	if (!isLoading) return null;
 	return (
 		<div className="overlay">
-			<PacmanLoader color="#FFFF00" size={32}/>
+			<CircleLoader color="#AAAAAA" size={64}/>
 		</div>
 	);
 };
