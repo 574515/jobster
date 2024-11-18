58e9b51c73140ed2ebb2d7fd4f81da69fe003350
574515 <574515c0d1ng@gmail.com>
2024-11-18 23:09:03 +0100

    Typization

--- frontend/src/components/AppActions.action.ts
+++ frontend/src/components/AppActions.action.ts
@@ -1,65 +1,114 @@
 import {
-	AllJobListingsResponseModel,
-	AllPoolListingsResponseModel,
-	JobCreationResponseModel,
-	JobDeletionResponseModel,
 	ModalSelectType,
+	MyConnectionResponseModel,
+	MyConnectionTransformedRequestModel,
+	MyFutureApplicationResponseModel,
+	MyFutureApplicationTransformedRequestModel,
+	MyJobResponseModel,
+	MyJobTransformedRequestModel,
 	PoolCreationResponseModel,
-	TransformedAddEditJobProps,
-	TransformedAddEditPoolType
-} from "../models/componentsTypes.ts";
-import {jobInstance} from "../api/axiosInstances.ts";
+} from "../models/types.ts";
+import {connectionInstance, jobInstance, toApplyInstance} from "../api/axiosInstances.ts";
+
+// TODO: Type checking
 
 export class JobActions {
-	static postNewJobListing = async (
-		data: TransformedAddEditJobProps
-	): Promise<JobCreationResponseModel> => {
-		const response = await jobInstance.post<JobCreationResponseModel>("/jobListings/newListing", data);
+	static postMyNewJob = async (
+		data: MyJobTransformedRequestModel
+	): Promise<MyJobResponseModel> => {
+		const response = await jobInstance.post<MyJobResponseModel>("", data);
 		return response.data;
 	}
 
-	static getAllJobListings = async (
-		userId: string,
-	): Promise<AllJobListingsResponseModel[]> => {
-		const response = await jobInstance.get<AllJobListingsResponseModel[]>(`/jobListings/all/${userId}`);
-		return response.data;
-	}
-
-	static changeJobStatus = async (
+	static changeMyJobStatus = async (
 		jobId: string,
 		status: ModalSelectType,
-	): Promise<JobCreationResponseModel> => {
-		const response = await jobInstance.put<JobCreationResponseModel>(`/jobListings/${jobId}/status`, status);
+	): Promise<MyJobResponseModel> => {
+		const response = await jobInstance.put<MyJobResponseModel>(`/${jobId}`, status);
 		return response.data;
 	}
 
-	static deleteJobListing = async (
+	static handleNote = async (
 		jobId: string,
-	): Promise<JobDeletionResponseModel> => {
-		const response = await jobInstance.delete<JobDeletionResponseModel>(`/jobListings/${jobId}`);
+		note: string | null = null,
+	): Promise<MyJobResponseModel> => {
+		const response = await jobInstance.put<MyJobResponseModel>(`/${jobId}/note`, {note});
+		return response.data;
+	}
+
+	static deleteMyJob = async (
+		jobId: string,
+	) => {
+		const response = await jobInstance.delete(`/${jobId}`);
+		return response.data;
+	}
+
+	static getAllMyJobs = async (
+		userId: string,
+	): Promise<MyJobResponseModel[]> => {
+		const response = await jobInstance.get<MyJobResponseModel[]>(`/${userId}`);
 		return response.data;
 	}
 }
 
-export class PoolActions {
-	static postNewPoolListing = async (
-		data: TransformedAddEditPoolType
+export class ConnectionActions {
+	static postMyConnection = async (
+		data: MyConnectionTransformedRequestModel
+	): Promise<MyConnectionResponseModel> => {
+		const response = await connectionInstance.post<MyConnectionResponseModel>("", data);
+		return response.data;
+	}
+
+	static handleNote = async (
+		connectionId: string,
+		note: string | null = null,
 	): Promise<PoolCreationResponseModel> => {
-		const response = await jobInstance.post<PoolCreationResponseModel>("/poolListings/newListing", data);
+		const response = await connectionInstance.put<PoolCreationResponseModel>(`/${connectionId}/note`, {note});
 		return response.data;
 	}
 
-	static getAllPoolListings = async (
-		userId: string,
-	): Promise<AllPoolListingsResponseModel[]> => {
-		const response = await jobInstance.get<AllPoolListingsResponseModel[]>(`/poolListings/all/${userId}`);
-		return response.data;
-	}
-
-	static deletePoolListing = async (
-		poolId: string,
+	static deleteMyConnection = async (
+		connectionId: string,
 	) => {
-		const response = await jobInstance.delete(`/poolListings/${poolId}`);
+		const response = await connectionInstance.delete(`/${connectionId}`);
+		return response.data;
+	}
+
+	static getAllMyConnections = async (
+		userId: string,
+	): Promise<MyConnectionResponseModel[]> => {
+		const response = await connectionInstance.get<MyConnectionResponseModel[]>(`/${userId}`);
+		return response.data;
+	}
+}
+
+export class ToApplyActions {
+	static postMyToApply = async (
+		data: MyFutureApplicationTransformedRequestModel
+	): Promise<MyFutureApplicationResponseModel> => {
+		const response = await toApplyInstance.post<MyFutureApplicationResponseModel>("", data);
+		return response.data;
+	}
+
+	static handleNote = async (
+		applicationId: string,
+		note: string | null = null,
+	): Promise<MyFutureApplicationResponseModel> => {
+		const response = await toApplyInstance.put<MyFutureApplicationResponseModel>(`/${applicationId}/note`, {note});
+		return response.data;
+	}
+
+	static deleteMyToApply = async (
+		applicationId: string,
+	) => {
+		const response = await toApplyInstance.delete(`/${applicationId}`);
+		return response.data;
+	}
+
+	static getAllMyFutureApplications = async (
+		userId: string,
+	): Promise<MyFutureApplicationResponseModel[]> => {
+		const response = await toApplyInstance.get<MyFutureApplicationResponseModel[]>(`/${userId}`);
 		return response.data;
 	}
 }
