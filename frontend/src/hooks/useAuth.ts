f640761be966a43eee0475b4b4419d5649924e02
574515 <574515c0d1ng@gmail.com>
2024-11-18 23:02:38 +0100

    Typization

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,13 +1,15 @@
 import React from 'react';
-import {AuthContextType} from "../models/contextTypes.ts";
+
 import AuthContext from '../context/AuthContext.tsx'
 
+import {AuthContextType} from "../models/types.ts";
+
 const useAuth = (): AuthContextType => {
-    const context = React.useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+	const context = React.useContext(AuthContext);
+	if (context === undefined) {
+		throw new Error('useAuth must be used within an AuthProvider');
+	}
+	return context;
 };
 
 export default useAuth;
