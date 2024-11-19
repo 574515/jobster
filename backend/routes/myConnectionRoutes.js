7efca0c7316646d88d7ff697e9cfd3dfc5ffeb52
574515 <574515c0d1ng@gmail.com>
2024-11-19 16:46:02 +0100

    Refactoring

--- backend/routes/myConnectionRoutes.js
+++ backend/routes/myConnectionRoutes.js
@@ -2,12 +2,18 @@ import express from 'express';
 
 import protectRoute from "../middleware/verifyJWT.js";
 
-import { addMyConnection, deleteMyConnection, getAllConnections } from '../controllers/myConnectionController.js'
+import {
+    addMyConnection,
+    deleteMyConnection,
+    getAllConnections,
+    handleNote
+} from '../controllers/myConnectionController.js'
 
 const router = express.Router();
 
 router.post("/myConnection", addMyConnection, protectRoute);
-router.get("/myConnection/:userId", deleteMyConnection, protectRoute);
-router.delete("/myConnection/:connectionId", getAllConnections, protectRoute);
+router.put("/myConnection/:connectionId/note", handleNote, protectRoute);
+router.delete("/myConnection/:connectionId", deleteMyConnection, protectRoute);
+router.get("/myConnection/:userId", getAllConnections, protectRoute);
 
 export default router;
--- backend/routes/myFutureApplicationRoutes.js
+++ backend/routes/myFutureApplicationRoutes.js
@@ -5,13 +5,15 @@ import protectRoute from "../middleware/verifyJWT.js";
 import {
     addMyFutureApplication,
     deleteMyFutureApplication,
-    getAllFutureApplications
+    getAllFutureApplications,
+    handleNote
 } from '../controllers/myFutureApplicationController.js'
 
 const router = express.Router();
 
-router.post("/myConnection", addMyFutureApplication, protectRoute);
-router.get("/myConnection/:userId", deleteMyFutureApplication, protectRoute);
-router.delete("/myConnection/:applicationId", getAllFutureApplications, protectRoute);
+router.post("/myFutureApplication", addMyFutureApplication, protectRoute);
+router.get("/myFutureApplication/:userId", getAllFutureApplications, protectRoute);
+router.put("/myFutureApplication/:applicationId/note", handleNote, protectRoute);
+router.delete("/myFutureApplication/:applicationId", deleteMyFutureApplication, protectRoute);
 
 export default router;
