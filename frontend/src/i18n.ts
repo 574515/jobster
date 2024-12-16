99af5e3cc573f07126726a0b08a5019d14518e94
574515 <574515c0d1ng@gmail.com>
2024-12-16 19:55:32 +0100

    Initialization for translation

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,24 +1,21 @@
 import i18n from 'i18next';
-
 import LanguageDetector from 'i18next-browser-languagedetector';
-import enGB from './locales/en-GB.json';
-import svSE from './locales/sv-SE.json';
 
+import {en_GB, sv_SE} from './locales/';
 import {initReactI18next} from 'react-i18next';
 
-i18n
+void i18n
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		resources: {
-			enGB: {translation: enGB},
-			svSE: {translation: svSE},
+			enGB: {translation: en_GB},
+			svSE: {translation: sv_SE},
 		},
 		lng: 'enGB',
 		fallbackLng: 'enGB',
-		interpolation: {
-			escapeValue: false
-		},
+		interpolation: {escapeValue: false},
+		keySeparator: '.',
 	});
 
 export default i18n;
